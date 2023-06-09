e0507372934036d442e35d44273da98eaa112511
Grigorii Pluzhnikov <pluzhnikovga@gmail.com>
2023-06-09 16:16:29 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ set -e
 git add .
 git commit -m 'deploy'
 
-git push -f https://github.com/PluzhnikovGA/vue-app.git master:gh-pages
+git push -f https://github.com/PluzhnikovGA/branchard.git master:gh-pages
 
 cd -
